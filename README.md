ee4761db951d1cb60562a5b5b1331e00338180dd
James Harrison <somtochukwujames2000@gmail.com>
2024-06-03 13:37:31 +0100

    chores: initializiation of repo

---
+++ README.md
@@ -0,0 +1,1 @@
+# Write and Deploy an NFT on AssetChain
---
+++ contracts/NFT.sol
@@ -0,0 +1,13 @@
+//SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0 <0.9.0;
+
+
+/**
+
+ * @author ASSETCHAIN DEVREL TEAM
+   @author James Harrison(https://github.com/KodeSage/)
+   @author  Okorie Ebube(https://github.com/theiceeman)
+ */
+contract NFT {
+	// State Variables
+}
